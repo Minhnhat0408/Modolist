8c275a007e92f16e9784815b7f636701ed319741
minhnn-3609 <nguyen.nhat.minh-b@sun-asterisk.com>
2026-03-26 10:15:40 +0900

    Add pwa

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -32,6 +33,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Script id="sw-register" src="/sw-register.js" strategy="afterInteractive" />
       </body>
     </html>
   );
